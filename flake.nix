50118a8375351d875df023ee3948f04a16b421fc
Cathal Mullan <contact@cathal.dev>
2025-02-01 03:03:28 +0000

    fork: dev environment

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "dropshot";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  # nix flake show
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+
+    let
+      perSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+
+      systemPkgs = perSystem (
+        system:
+
+        import nixpkgs {
+          inherit system;
+
+          overlays = [
+            (import rust-overlay)
+          ];
+        }
+      );
+
+      perSystemPkgs = f: perSystem (system: f (systemPkgs.${system}));
+    in
+    {
+      devShells = perSystemPkgs (pkgs: {
+        # nix develop
+        default = pkgs.mkShell {
+          name = "dropshot-shell";
+
+          env = {
+            NIX_PATH = "nixpkgs=${nixpkgs.outPath}";
+          };
+
+          buildInputs = with pkgs; [
+            # Rust
+            (rust-bin.stable."1.84.0".minimal.override {
+              extensions = [
+                "clippy"
+                "rust-analyzer"
+                "rust-docs"
+                "rust-src"
+                "rustfmt"
+              ];
+            })
+            pkg-config
+            openssl
+
+            # TOML
+            taplo
+
+            # Nix
+            nixfmt-rfc-style
+            nixd
+            nil
+          ];
+        };
+      });
+    };
+}
